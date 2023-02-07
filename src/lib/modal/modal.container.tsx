b3f652546f2ac005fd450ed95a5ea2e3d6cf0c08
dotoli <dotoli.dev@gmail.com>
2023-02-07 14:00:05 +0900

    fix: nextjs error

--- src/lib/modal/modal.container.tsx
+++ src/lib/modal/modal.container.tsx
@@ -5,13 +5,10 @@ import { useModal } from "./modal.hook";
 export const ModalContainer = () => {
   const modals = useModal();
 
+  if (modals.size === 0) return null;
+
   return (
-    <div
-      className={clsx({
-        "dev-waca__react-modal--container-visible": modals.size > 0,
-        "dev-waca__react-modal--container-hidden": modals.size === 0,
-      })}
-    >
+    <div className={clsx("dev-waca__react-modal--container-visible")}>
       {Array.from(modals.values()).map(
         ([Modal, { id, backgroundCloseable, props }]) => (
           <ModalWrapper
